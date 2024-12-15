b4b288a269a07ca6e3c77ecc562ebd41d6266108
chammem sinda <99534039+chammem@users.noreply.github.com>
2024-12-15 23:19:32 +0100

    6 commit

--- src/main/java/tn/esprit/foyer/configuration/AspectConfig.java
+++ src/main/java/tn/esprit/foyer/configuration/AspectConfig.java
@@ -2,18 +2,34 @@ package tn.esprit.foyer.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Component
 @Aspect
 @Slf4j
 public class AspectConfig {
-
+    @After("execution(* tn.esprit.foyer.service.*.*(..))")
     @Before("execution(* tn.esprit.foyer.service.*.*(..))")
     public void logMethodEntry(JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
         log.info("In method " + name + " : ");
 }
+    @Around("execution(* tn.esprit.foyer.service.*.*(String,..)) throws Exception")
+    void perfermance(ProceedingJoinPoint pjp) throws Throwable
+    {
+        LocalDateTime debut = LocalDateTime.now();
+        Object o= pjp.proceed();
+        LocalDateTime fin = LocalDateTime.now();
+        long duree=(fin.getSecond()-debut.getSecond());
+        log.info("temp execution : "+duree);
+    }
+
 }
